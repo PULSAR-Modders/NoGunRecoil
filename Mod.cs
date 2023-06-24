0ddad8e3b14026ef0dfa2695de61fcd4b90712af
18107 <18107@users.noreply.github.com>
2023-06-24 19:15:15 +0930

    Add files via upload

---
+++ Mod.cs
@@ -0,0 +1,22 @@
+﻿using PulsarModLoader;
+
+namespace No_gun_recoil
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.0.0";
+
+        public override string Author => "18107";
+
+        public override string ShortDescription => "Removes gun recoil";
+
+        public override string Name => "No gun recoil";
+
+        public override string ModID => "nogunrecoil";
+
+        public override string HarmonyIdentifier()
+        {
+            return "id107.nogunrecoil";
+        }
+    }
+}
---
+++ PawnItem.cs
@@ -0,0 +1,69 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No_gun_recoil
+{
+    internal class PawnItem
+    {
+        [HarmonyPatch(typeof(PLPawnItem_BurstPistol), "FireShot")]
+        class BurstPistol
+        {
+            static void Postfix(PLPawn ___MySetupPawn)
+            {
+                ___MySetupPawn.VerticalMouseLook.RotationY_AddOverTime -= 1.25f;
+                ___MySetupPawn.VerticalMouseLook.RotationY -= 0.5f;
+            }
+        }
+
+        [HarmonyPatch(typeof(PLPawnItem_HandShotgun), "InternalFireShot")]
+        class HandShotgun
+        {
+            static void Postfix(PLPawn ___MySetupPawn)
+            {
+                ___MySetupPawn.VerticalMouseLook.RotationY_AddOverTime -= 7f;
+            }
+        }
+
+        [HarmonyPatch(typeof(PLPawnItem_HeavyPistol), "FireShot")]
+        class HeavyPistol
+        {
+            static void Postfix(PLPawn ___MySetupPawn)
+            {
+                ___MySetupPawn.VerticalMouseLook.RotationY_AddOverTime -= 0.5f;
+            }
+        }
+
+        [HarmonyPatch(typeof(PLPawnItem_PhasePistol), "FireShot")]
+        class PhasePistol
+        {
+            static void Postfix(PLPawn ___MySetupPawn)
+            {
+                ___MySetupPawn.VerticalMouseLook.RotationY_AddOverTime -= 0.3f;
+            }
+        }
+
+        [HarmonyPatch(typeof(PLPawnItem_VortexGrenade), "FireShot")]
+        class VortexGrenade
+        {
+            static void Postfix(PLPawn ___MySetupPawn)
+            {
+                ___MySetupPawn.VerticalMouseLook.RotationY_AddOverTime -= 12f;
+                ___MySetupPawn.VerticalMouseLook.RotationY -= 2f;
+            }
+        }
+
+        [HarmonyPatch(typeof(PLPawnItem_WDHeavy), "FireShot")]
+        class WDHeavy
+        {
+            static void Postfix(PLPawn ___MySetupPawn)
+            {
+                ___MySetupPawn.VerticalMouseLook.RotationY_AddOverTime -= 6f;
+                ___MySetupPawn.VerticalMouseLook.RotationY -= 0.5f;
+            }
+        }
+    }
+}
